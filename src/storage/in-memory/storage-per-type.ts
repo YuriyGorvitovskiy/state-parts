cee5cf74fc98efa2e8a7c06284dcf0317e6d6e50
Yuriy Gorvitovskiy <yuriy@gorvitovskiy.com>
2019-04-26 22:44:22 -0700

    adding filter to storage

--- src/storage/in-memory/storage-per-type.ts
+++ src/storage/in-memory/storage-per-type.ts
@@ -32,14 +32,44 @@ export class StoragePerType implements Glue.IPatchConsumer, Glue.IEntityProvider
             throw new Error("Wrong selector type: '" + selector.type + "', expecting '" + this.type + "'.");
         }
 
-        const result: Glue.IEntity[] = [];
+        let result = this.filterByIds(selector.ids);
+        result = result.filter((e) => this.checkFilters(e, selector.filters));
+
         return result;
     }
+
+    private filterByIds(ids: string[]): Glue.IEntity[] {
+        if (null == ids || 0 === ids.length) {
+            return  Object.values(this.entites);
+        }
+        const result: Glue.IEntity[] = [];
+        ids.forEach((id) => {
+            const entity = this.entites[id];
+            if (entity) {
+                result.push(entity);
+            }
+        });
+        return result;
+    }
+
+    private checkFilters(entity: Glue.IEntity, filters: Glue.IFilter[]): boolean {
+        return null == filters.find((f) => !this.checkFilter(entity, f));
+    }
+
 
     private checkFilter(entity: Glue.IEntity, filter: Glue.IFilter): boolean {
         const value = entity.attributes[filter.attribute];
         switch(filter.operation) {
-
+            case Glue.ComparisonOperation.EQUAL:    return value === filter.values[0];
+            case Glue.ComparisonOperation.IN:       return filter.values.indexOf(value) >= 0;
+            case Glue.ComparisonOperation.LESS:     return value < filter.values[0];
+            case Glue.ComparisonOperation.MORE:     return value > filter.values[0];
+            case Glue.ComparisonOperation.BETWEEN:  return filter.values[0] < value  && value < filter.values[1];
+            case Glue.ComparisonOperation.NOT_EQUAL:    return value !== filter.values[0];
+            case Glue.ComparisonOperation.NOT_IN:       return filter.values.indexOf(value) < 0;
+            case Glue.ComparisonOperation.NOT_LESS:     return value >= filter.values[0];
+            case Glue.ComparisonOperation.NOT_MORE:     return value <= filter.values[0];
+            case Glue.ComparisonOperation.NOT_BETWEEN:  return value < filter.values[0] || filter.values[1] < value;
         }
         return false;
     }
